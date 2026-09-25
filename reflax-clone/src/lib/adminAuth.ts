864834700e47afa37ae116bba4b575b7a5c296e1
Habib ullah <khalidameer367@gmail.com>
2026-09-25 10:06:47 +0500

    Delete reflax-clone directory

--- reflax-clone/src/lib/adminAuth.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function isAdminAuthed(req: NextRequest): boolean {
-  const cookie = req.cookies.get("reflax_admin")?.value;
-  return Boolean(cookie) && cookie === process.env.ADMIN_PASSWORD;
-}
